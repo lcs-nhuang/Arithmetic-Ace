1ecefc018dc72532e41620f52e4a4fd708b70448
Nicole Huang <nhuang@lcs.on.ca>
2022-02-07 08:52:45 -0500

    Initial Commit

---
+++ Arithmetic Ace/Arithmetic_AceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_AceApp.swift
+//  Arithmetic Ace
+//
+//  Created by Nicole on 2022/2/7.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_AceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
