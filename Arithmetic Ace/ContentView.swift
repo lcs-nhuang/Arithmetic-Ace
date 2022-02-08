557bb74947611451c148f2bdf6dac78a6c364b1a
Nicole Huang <nhuang@lcs.on.ca>
2022-02-08 12:07:02 -0500

    Rearrange the code.

--- Arithmetic Ace/ContentView.swift
+++ Arithmetic Ace/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     //MARK: Stored properties
     @State var multiplicand = Int.random(in: 1...12)
     @State var multiplier = Int.random(in: 1...12)
-   //This string contain whatever we put in
+    //This string contain whatever we put in
     @State var inputGiven = ""
     
     //Has an answer been checked?
@@ -29,16 +29,16 @@ struct ContentView: View {
     var body: some View {
         
         VStack(spacing:0){
-        HStack{
-          Text("✕")
-          
-            Spacer()
-            
-            VStack(alignment: .trailing){
-                Text("\(multiplicand)")
-                Text("\(multiplier)")
+            HStack{
+                Text("✕")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing){
+                    Text("\(multiplicand)")
+                    Text("\(multiplier)")
+                }
             }
-        }
             
             Divider()
             
@@ -52,70 +52,75 @@ struct ContentView: View {
                     
                     Image(systemName: "x.circle")
                         .foregroundColor(.red)
-    //  Condition1             and Conditional2        true  false
-.opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
-                        
+                    //  Condition1 and Conditional2 true  false
+                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                    
                 }
-               
-        
+                
+                
                 Spacer()
+                
                 
                 TextField("",
                           text: $inputGiven)
                     .multilineTextAlignment(.trailing)
             }
             
+            
+            
             ZStack{
-            Button(action: {
-                
-                // Answer has been checked!
-                answerChecked = true
-                
-                // Convert the input given to an integer, if  possible
-                guard let productGiven = Int(inputGiven) else {
-                    //Sadness, not a number
-                    return
-                }
-                
-                //Check the anwser!
-                if productGiven == correctProduct {
-                    //Celebrate!
-                    answerCorrect = true
-                }
-                else{
-                    //sadness, they are incorrect
-                }
-            }, label: {
-                Text("Check Answer")
-                    .font(.largeTitle)
-            })
-                .padding()
-                .buttonStyle(.bordered)
-                .opacity(answerChecked == true ? 0.0 : 1.0)
-            
-            
-            
-            
-            Button(action: {
-                //Generate a new question
-                multiplicand = Int .random(in: 1...12)
-                multiplier = Int .random(in: 1...12)
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if  possible
+                    guard let productGiven = Int(inputGiven) else {
+                        //Sadness, not a number
+                        return
+                    }
+                    
+                    //Check the anwser!
+                    if productGiven == correctProduct {
+                        //Celebrate!
+                        answerCorrect = true
+                    }
+                    else{
+                        //sadness, they are incorrect
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                //Only show this buttin when the anwser has not been checked
+                    .opacity(answerChecked == true ? 0.0 : 1.0)
                 
                 
-                //Reset properties that track
-                answerCorrect = false
-                answerChecked = false
                 
-                //Rest input field
-                inputGiven = ""
-            }, label: {
-                Text("New question")
-                    .font(.largeTitle)
-            })
-                .padding()
-                .buttonStyle(.bordered)
-                .opacity(answerChecked == true ? 1.0 : 0.0)
-            
+                
+                Button(action: {
+                    //Generate a new question
+                    multiplicand = Int .random(in: 1...12)
+                    multiplier = Int .random(in: 1...12)
+                    
+                    
+                    //Reset properties that track
+                    answerCorrect = false
+                    answerChecked = false
+                    
+                    //Rest input field
+                    inputGiven = ""
+                }, label: {
+                    Text("New question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                //Only show this button when an anwser has been checked
+                    .opacity(answerChecked == true ? 1.0 : 0.0)
+                
             }
             
             
